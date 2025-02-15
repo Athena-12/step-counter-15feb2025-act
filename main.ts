d829e7e653b9595aba17fbf673caf8486f2b74eb
Athena-12 <cabugon.eloisa@gmail.com>
2025-02-15 11:12:20 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.Shake, function () {
+    STEP += 1
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showNumber(STEP)
+})
+let STEP = 0
+STEP = 0
+basic.forever(function () {
+	
+})
